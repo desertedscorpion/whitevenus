f1c4217dd91faebf1732dcd7d1eb04d8c1f3e9fa
Emory Merryman <emory.merryman+sBcXTfGowPLKz3XB@gmail.com>
2016-03-29 04:35:56 -0400

    all tests pass

--- testing.sh
+++ testing.sh
@@ -5,7 +5,6 @@ EXECUTABLE=./phonetic.sh &&
     then
 	EXECUTABLE=${@}
     fi &&
-    echo EXEC: ${EXECUTABLE}
     function test {
 	INPUT=${1} &&
 	    EXPECTED=${2} &&
@@ -24,5 +23,16 @@ EOF
 	    fi &&
 	    true
     } &&
-    test ABC "Alfa Beta" &&
+    test ABC "ALFA BRAVO CHARLIE  " &&
+    test ABCDEFGHIJKLMNOPQRSTUVWXYZ "ALFA BRAVO CHARLIE DELTA ECHO FOXTROT GOLF HOTEL INDIA JULIET KILO LIMA MIKE NOVEMBER OSCAR PAPA QUEBEC ROMEO SIERRA TANGO UNIFORM VICTOR WHISKEY XRAY YANKEE ZULU  " &&
+    test abcdefghijklmnopqrstuvwxyz "alfa bravo charlie delta echo foxtrot golf hotel india juliet kilo lima mike november oscar papa quebec romeo sierra tango uniform victor whiskey xray yankee zulu  " &&
+    test 1234567890 "One Two Three Four Five Six Seven Eight Nine Zero  " &&
+    test "!@#$%^&*()" "Bang At Pound Dollar Percent Caret Ampersand Asterisk OpenParenthesis CloseParenthesis  " &&
+    test "--+=" "Minus Minus Plus Equals  " &&
+    test "{[]}" "OpenBrace OpenBracket CloseBracket CloseBrace  " &&
+    test "\\\\|" "BackSlash Pipe  " &&
+    test ";:\"\'" "Semicolon Colon DoubleQuote SingleQuote  " &&
+    test "<,.>" "LessThan Comma Period GreaterThan  " &&
+    test "?/" "Question ForwardSlash  " &&
+    echo ALL TESTS PASSED &&
     true
